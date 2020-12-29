f84079ae507e7f989d2b954d7b21323723ee0e8d
Scott Balun <sbalun@gmail.com>
2020-12-29 00:03:09 -0500

    Initial commit

---
+++ lists-med-ins-project.py
@@ -0,0 +1,62 @@
+# Python List Homework - Codecademy Medical Insurance Project
+# Scott Balun
+# December 28, 2020
+#
+# In this lesson I will learn about the following string methods:
+#
+
+####   Helper Methods   ####
+def print_output_seperator(example_number = 99999):
+    print("# ----------------")
+    print("# Problem " + str(example_number) + " Output")
+    print("# ----------------")
+####    ####    ####    ####
+
+#
+print_output_seperator()
+# ------------
+# Problem X - Xx
+# ------------
+# Problem Statement: Xx
+
+#
+print_output_seperator()
+# ------------
+# Problem X - Xx
+# ------------
+# Problem Statement: Xx
+
+#
+print_output_seperator()
+# ------------
+# Problem X - Xx
+# ------------
+# Problem Statement: Xx
+
+#
+print_output_seperator()
+# ------------
+# Problem X - Xx
+# ------------
+# Problem Statement: Xx
+
+#
+print_output_seperator()
+# ------------
+# Problem X - Xx
+# ------------
+# Problem Statement: Xx
+
+#
+print_output_seperator()
+# ------------
+# Problem X - Xx
+# ------------
+# Problem Statement: Xx
+
+#
+print_output_seperator()
+# ------------
+# Problem X - Xx
+# ------------
+# Problem Statement: Xx
