d1f1670a43c765eca539ddccd65ad41d2f99ed30
Scott Balun <sbalun@gmail.com>
2021-01-06 14:27:40 -0500

    Completed all steps

--- dict-med-ins-project.py
+++ dict-med-ins-project.py
@@ -27,7 +27,7 @@ print(medical_costs)
 
 # Update the value associated with Vinay to 3325.0.  Print the updated dictionary.
 medical_costs["Vinay"] = 3325.0
-print(medical_costs)
+print("\n", medical_costs)
 
 # Create a variable called total_cost and set it equal to 0.  Iterate through the values in medical_costs and add each value to the total_cost variable.
 # After the loop, create a variable called average_cost that stores the total_cost divided by the length of the medical_costs dictionary.
@@ -39,7 +39,7 @@ for key, value in medical_costs.items():
     total_cost += value
 
 average_cost = total_cost / len(medical_costs)
-print("Average Insurance Cost: {}".format(str(average_cost)))
+print("\nAverage Insurance Cost: {}".format(str(average_cost)))
 
 # Create two lists called names and ages with the following data:
 #   names	    ages
@@ -53,5 +53,59 @@ names = ['Marina','Vinay','Connie','Isaac','Valentina']
 ages = [27,24,43,35,52]
 
 # Create a variable called zipped_ages that is a zipped list of pairs between the names list and the ages list.
+zipped_ages = zip(names,ages)
+
+# Create a dictionary called names_to_ages by using a list comprehension that iterates through
+# zipped_ages and turns each pair into a key : value item.  Print names_to_ages to see the result.
+
+names_to_ages = {names: ages for (names, ages) in zipped_ages}
+print("\nThe values in the dict, names_to_ages, are: ", names_to_ages)
+
+# Use .get() to get the value of Marina’s age and store it in a variable called marina_age. 
+# Use None as a default value if the key doesn’t exist. 
+# Print marina_age with the following message: 'Marina's age is {marina_age}'
+mariana_age  = names_to_ages['Marina']
+print("\nMarina's age is {}".format(mariana_age))
+
+# Create an empty dictionary called medical_records
+medical_records = {}
+
+# Add "Marina" to medical_records as a key with the value being a dictionary of medical data:
+# {"Age": 27, "Sex": "Female", "BMI": 31.1, "Children": 2, "Smoker": "Non-smoker", "Insurance_cost": 6607.0}
+medical_records["Marina"] = {"Age": 27, "Sex": "Female", "BMI": 31.1, "Children": 2, "Smoker": "Non-smoker", "Insurance_cost": 6607.0}
+
+# Add the following individuals:
+#
+# Name	    Age	Sex	    BMI	    Children	Smoker	    Insurance Cost
+# Vinay	    24	Male	26.9	0	        Non-smoker	3225.0
+# Connie	43	Female	25.3	3	        Non-smoker	8886.0
+# Isaac	    35	Male	20.6	4	        Smoker	    16444.0
+# Valentina	52	Female	18.7	1	        Non-smoker	6420.0
+
+medical_records["Vinay"] = {"Age": 24, "Sex": "Male", "BMI": 26.9, "Children": 0, "Smoker": "Non-smoker", "Insurance_cost": 3225.0}
+medical_records["Connie"] = {"Age":43, "Sex": "Female", "BMI": 25.3, "Children": 3, "Smoker": "Non-smoker", "Insurance_cost": 8886.0}
+medical_records["Isaac"] = {"Age": 35, "Sex": "Male", "BMI": 20.6, "Children": 4, "Smoker": "Smoker", "Insurance_cost": 16444.0}
+medical_records["Valentina"] = {"Age":52, "Sex": "Female", "BMI": 18.7, "Children": 1, "Smoker": "Non-smoker", "Insurance_cost": 6420.0} 
+
+# Print medical_records to see the result
+print("\n", medical_records)
+
+# The medical_records dictionary acts like a database of medical records. Let’s access a specific piece of data in medical_records.
+# Print out Connie’s insurance cost with the following message: "Connie's insurance cost is X dollars."
+print("\nConnie's insurance cost is ${}0 dollars.\n".format(medical_records["Connie"]["Insurance_cost"]))
+
+# Remove Vinay from medical_records.
+medical_records.pop("Vinay")
+
+# Use a for loop to iterate through the items of medical_records. 
+# For each key-value pair, print out a string that looks like the following:
+#   {Name} is a {Age} year old {Sex} {Smoker} with a BMI of {BMI} and insurance cost of {Insurance_cost}
+
+for key, value in medical_records.items():
+    #print(key,value['Age'])
+    print("{} is a {} year old {} {} with a BMI of {} and a insurance cost of ${}0".format(key, value['Age'], value['Sex'], value['Smoker'], value['BMI'], value['Insurance_cost']))
+
+# Extra Credit: Create a function called update_medical_records() that takes in the name of an individual
+# as well as their medical data, and then updates the medical_records dictionary accordingly.
 
 
